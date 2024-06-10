291d3f626618dbaff713fcccb6f6b9202d9589fe
ceasinugo <ceasinugo@wustl.edu>
2024-06-10 09:32:50 -0500

    [Part 1] Graded Assignment #1: Candidate Testing

---
+++ candidate-testing-task1.js
@@ -0,0 +1,63 @@
+const input = require('readline-sync');
+
+// TODO 2: modify your quiz app to ask 5 questions //
+
+// TODO 1.1a: Define candidateName. Declare this variable as an empty string. // 
+let candidateName = "";
+
+// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
+
+
+//TODO: Variables for Part 2
+let questions;
+let correctAnswers;
+let candidateAnswers;
+
+
+function askForName() {
+  // TODO 1.1b: Ask for candidate's name //
+    candidateName = input.question("What is your name (First Name Last Name)? ");
+}
+
+function askQuestion() {
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+    candidateAnswer = input.question(question);
+}
+
+function gradeQuiz(candidateAnswers) {
+
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+    console.log(`Your answer: ${candidateAnswer}`);
+    console.log(`Correct answer: ${correctAnswer}`);
+    
+  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+
+
+    return grade;
+}
+
+function runProgram() {
+    askForName();
+  // TODO 1.1c: Greet candidate using their name //
+    console.log(`Welcome to the other side, ${candidateName} \n.`);
+    askQuestion();
+    gradeQuiz(this.candidateAnswers);
+}
+
+runProgram()
+
+// ----------- Don't write any code or change any code below this line ---------- //
+module.exports = {
+  candidateName: candidateName,
+  question: question,
+  correctAnswer: correctAnswer,
+  candidateAnswer: candidateAnswer,
+  questions: questions,
+  correctAnswers: correctAnswers,
+  candidateAnswers: candidateAnswers,
+  gradeQuiz: gradeQuiz,
+  runProgram: runProgram
+};
